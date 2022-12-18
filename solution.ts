9aef8a4fc099ed644d12c55040754bfb40e8fb60
Kooreis <kooreis.official@gmail.com>
2022-12-18 09:48:17 -0600

    This update introduces the WordSegmentation class and its constructor. The constructor takes an array of strings (dictionary) as an argument and converts it into a Set for faster lookup.

--- solution.ts
+++ solution.ts
@@ -1,35 +1,7 @@
-Here is a TypeScript solution for the problem:
-
-```typescript
 class WordSegmentation {
     dictionary: Set<string>;
 
     constructor(dictionary: string[]) {
         this.dictionary = new Set(dictionary);
     }
-
-    canSegmentString(input: string): boolean {
-        const memo: boolean[] = new Array(input.length + 1).fill(false);
-        memo[0] = true;
-
-        for (let i = 1; i <= input.length; i++) {
-            for (let j = 0; j < i; j++) {
-                if (memo[j] && this.dictionary.has(input.substring(j, i))) {
-                    memo[i] = true;
-                    break;
-                }
-            }
-        }
-
-        return memo[input.length];
-    }
 }
-
-const dictionary = ['apple', 'pear', 'pier', 'pie'];
-const wordSegmentation = new WordSegmentation(dictionary);
-
-console.log(wordSegmentation.canSegmentString('applepie'));  // true
-console.log(wordSegmentation.canSegmentString('applepeer'));  // false
-```
-
-This solution uses dynamic programming to solve the problem. It iterates over the input string and checks if each substring can be segmented into words from the dictionary. The `canSegmentString` method returns `true` if the string can be segmented and `false` otherwise.
