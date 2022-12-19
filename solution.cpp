8e2967e978660d154c39c1825e8aa347a527eab9
Kooreis <kooreis.official@gmail.com>
2022-12-19 10:48:17 -0600

    In this update, the function 'canSegment' is defined. This function checks if a given string can be segmented into words that are present in a given dictionary. It uses dynamic programming to solve the problem.

--- solution.cpp
+++ solution.cpp
@@ -1,4 +1,17 @@
-#include <iostream>
-#include <unordered_set>
-#include <vector>
-#include <string>
+bool canSegment(std::string s, std::unordered_set<std::string>& dict) {
+    int n = s.size();
+    std::vector<bool> dp(n + 1, false);
+    dp[0] = true;
+    for (int i = 1; i <= n; i++) {
+        for (int j = i - 1; j >= 0; j--) {
+            if (dp[j]) {
+                std::string word = s.substr(j, i - j);
+                if (dict.find(word) != dict.end()) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+    }
+    return dp[n];
+}
